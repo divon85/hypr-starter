298b8b406c1b0ee326be273a491fc33088eee291
divon85 <94886650+divon85@users.noreply.github.com>
2024-04-24 14:14:15 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,14 @@
 version=$(cat .version/name)
 clear
 
+# Set installation mode
+mode="live"
+if [ ! -z $1 ]; then
+    mode="dev"
+    echo "IMPORTANT: DEV MODE ACTIVATED. "
+    echo "Existing dotfiles folder will not be modified."
+    echo "Symbolic links will not be created."
+fi
 source .install/colors.sh
 source .install/library.sh
 source .install/required.sh
